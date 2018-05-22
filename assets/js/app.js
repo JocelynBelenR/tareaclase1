4bbd2c9be9998b273e6fbe44a2c0fc5feb200884
Carolinadelb <carolinadelgadob@gmail.com>
2018-05-22 19:58:56 -0400

    Cambio en el botón de datos curiosos de breakin bad

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,20 @@
-//Nombre en HTML + Bienvenida 
+window.onload = function (){
+
+    //Nombre en HTML + Bienvenida 
 function functionWelcome (){
     var user = document.getElementById("name").value;
     var p = document.getElementById("welcomeMsje");
     p.innerHTML = "Bienvenida " + user;
+}
+
+//Boton para breaking bad 
+var btn = document.getElementById("button-brb")
+//Texto de datos curioso
+var txt = document.getElementById ("datoC");
+
+
+btn.addEventListener('click', function(){
+       
+    txt.innerHTML = "<h2>Git & Github te amamos </h2>";
+})
 }
