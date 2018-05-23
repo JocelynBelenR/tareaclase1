0c69105ab4b6c4822dc3b0a1e7fb6c4f82ca3fd8
Carolinadelb <carolinadelgadob@gmail.com>
2018-05-22 20:32:09 -0400

    boton con foto que funciona más o menos

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,13 +8,15 @@ function functionWelcome (){
 }
 
 //Boton para breaking bad 
-var btn = document.getElementById("button-brb")
+var btn = document.getElementById("button-brb");
 //Texto de datos curioso
 var txt = document.getElementById ("datoC");
-
+//foto
+var img = document.getElementById ("pic");
 
 btn.addEventListener('click', function(){
        
-    txt.innerHTML = "<h2>Git & Github te amamos </h2>";
+    txt.innerHTML = "<p> Anna Gunn es la actriz que dio vida al personaje de Skyler White, años más tarde escribiría en The New York Times, luego del final de la serie, un interesantísimo artículo en que habla sobre el odio que le profesan a su personaje... y a ella misma. Explica que se trata de una especie </p>";
+    document.getElementById("pic").src = "breaking-bad.jpg";
 })
 }
